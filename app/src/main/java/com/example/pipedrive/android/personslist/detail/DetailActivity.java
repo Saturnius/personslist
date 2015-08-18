3e3dace92a2731e8b8e79c7c2df28989588fad1b
Julik Julik <saturnius@yandex.ru>
2015-08-18 12:52:40 +0300

    spacing

--- app/src/main/java/com/example/pipedrive/android/personslist/detail/DetailActivity.java
+++ app/src/main/java/com/example/pipedrive/android/personslist/detail/DetailActivity.java
@@ -3,8 +3,6 @@ package com.example.pipedrive.android.personslist.detail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-
 import com.example.pipedrive.android.personslist.R;
 
 
